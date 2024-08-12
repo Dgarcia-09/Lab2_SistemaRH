0cdab8c1d6d4fa1a20c94544ac019d8eaf5413f1
Diego Garcia <dgarciaowo@gmail.com>
2024-08-11 18:19:29 -0600

    feat: se termino la iterface del servicio de empleado

--- src/main/java/com/diegogarcia/system/services/EmpleadoService.java
+++ src/main/java/com/diegogarcia/system/services/EmpleadoService.java
@@ -1,5 +1,34 @@
 package com.diegogarcia.system.services;
 
-public class EmpleadoService {
+import java.util.List;
+
+import com.diegogarcia.system.model.Empleados;
+
+public class EmpleadoService implements IEmpleadoService{
+
+    @Override
+    public List<Empleados> listarEmpleados() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarEmpleados'");
+    }
+
+    @Override
+    public Empleados buscarEmpleado(Integer idEmpelado) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarEmpleado'");
+    }
+
+    @Override
+    public Empleados guardarEmpleado(Empleados empleados) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardarEmpleado'");
+    }
+
+    @Override
+    public void eliminarEmpleado(Empleados empleados) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarEmpleado'");
+    }
+
 
 }
--- src/main/java/com/diegogarcia/system/services/IEmpleadoService.java
+++ src/main/java/com/diegogarcia/system/services/IEmpleadoService.java
@@ -1,5 +1,17 @@
 package com.diegogarcia.system.services;
 
+import java.util.List;
+
+import com.diegogarcia.system.model.Empleados;
+
 public interface IEmpleadoService {
+
+    public List<Empleados> listarEmpleados();
+
+    public Empleados buscarEmpleado (Integer idEmpelado);
+
+    public Empleados guardarEmpleado (Empleados empleados);
+
+    public void eliminarEmpleado (Empleados empleados);
 
 }

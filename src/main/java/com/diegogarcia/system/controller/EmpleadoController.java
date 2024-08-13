98c22827b4da60c9debf194a19dc58f901a1221a
Diego Garcia <dgarciaowo@gmail.com>
2024-08-12 19:12:33 -0600

    feat: se agrego el metodo agregar al controlador de empleados

--- src/main/java/com/diegogarcia/system/controller/EmpleadoController.java
+++ src/main/java/com/diegogarcia/system/controller/EmpleadoController.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +36,17 @@ public class EmpleadoController {
         empleado2.forEach((empleado -> logger.info(empleado.toString())));
         return empleado2;
 
-        
+    }
+
+
+    @PostMapping("/empleadosA")
+
+    public Empleados agregarEmpleado(@RequestBody Empleados empleados){
+        logger.info("Empleado agregado");
+        return iEmpleadoService.guardarEmpleado(empleados);
+
+
+
     }
 
 

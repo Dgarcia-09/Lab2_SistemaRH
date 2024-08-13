1dcb3556bea29edde8764f565b88ee825ddff22b
Informatica <informatica@alumno.local>
2024-08-13 08:37:32 -0600

    feat: se agrego el metodo eliminar al controlador de empleados

--- src/main/java/com/diegogarcia/system/controller/EmpleadoController.java
+++ src/main/java/com/diegogarcia/system/controller/EmpleadoController.java
@@ -1,11 +1,14 @@
 package com.diegogarcia.system.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,10 +62,11 @@ public class EmpleadoController {
     }
 
     @PutMapping("/empleados/{id}")
-    public ResponseEntity <Empleados> editarEmpleados(@PathVariable Integer id, @RequestBody Empleados empleadosRecibido) {
+    public ResponseEntity<Empleados> editarEmpleados(@PathVariable Integer id,
+            @RequestBody Empleados empleadosRecibido) {
         Empleados empleados = iEmpleadoService.buscarEmpleado(id);
-        if(empleados == null)
-        throw new EmpleadoException("El id recibido no existe");
+        if (empleados == null)
+            throw new EmpleadoException("El id recibido no existe");
         empleados.setDpi(empleadosRecibido.getDpi());
         empleados.setNombreCompleto(empleadosRecibido.getNombreCompleto());
         empleados.setPuesto(empleadosRecibido.getPuesto());
@@ -70,6 +74,23 @@ public class EmpleadoController {
         iEmpleadoService.guardarEmpleado(empleados);
         return ResponseEntity.ok(empleados);
     }
+    
+
+    @DeleteMapping("/empleados/{id}")
+
+    public ResponseEntity<Map<String, Boolean>> eliminarEmpleado(@PathVariable Integer id) {
+        Empleados empleados = iEmpleadoService.buscarEmpleado(id);
+        if (empleados == null)
+            throw new EmpleadoException("El empleado no existe");
+        iEmpleadoService.eliminarEmpleado(empleados);
+
+        //JSON {eliminado = true}
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("Eliminado", true);
+        return ResponseEntity.ok(respuesta);
+    
+    }
+
 }
 
 
